482034082db33a091939a2689f2296dede6e1648
Ryan Wilkie <ryanwilkie4@gmail.com>
2016-09-07 16:12:34 +0100

    create connection with DVLA API

--- callAPIDev.php
+++ callAPIDev.php
@@ -1,5 +1,19 @@
 <?php
 
-    
+    $regno = str_replace(' ', '', $_GET["regno"]);
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => "https://uk1.ukvehicledata.co.uk/api/datapackage/VehicleData?key_vrm=" . $regno,
+        CURLOPT_HTTPHEADER => array("Authorization: ukvd-ipwhitelist 67a18a66-c24d-43ab-9ac3-b85e75ffd2cc")
+    ));
+
+    $resp = curl_exec($curl);
+
+    curl_close($curl);
+
+    var_dump($resp);
 
  ?>
